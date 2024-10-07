1a3b3008708d2f4e57c7b3f10aa64a3701c7b5a6
deedoth <ayoooladamilare@gmail.com>
2024-10-07 12:25:59 +0100

    SpendingBlock.tsx file added

---
+++ components/SpendingBlock.tsx
@@ -0,0 +1,81 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SpendingType } from '@/types'
+import Colors from '@/constants/Colors'
+import { AirbnbIcon, AmazonIcon, DollarSignIcon, FigmaIcon, NetflixIcon, ShoppingCartIcon, SpotifyIcon } from '@/constants/icons'
+
+export default function SpendingBlock({spendingList} : {spendingList: SpendingType[]}) {
+    let icon = <DollarSignIcon width={22} height={22} color={Colors.white} /> 
+  return (
+    <View style={styles.spendingSectionWrapper}>
+      <Text style={styles.sectionTitle}>
+        July <Text style={{fontWeight:'700'}}> Spending</Text>
+    </Text>
+    
+    {spendingList.map((item) => {
+        if (item.name == "AirBnb Rent") {
+            icon = <AirbnbIcon width={22} height={22} color={Colors.white} />
+        } else if ( item.name == "Netflix") {
+            icon = <NetflixIcon width={22} height={22} color={Colors.white} />
+        } else if (item.name == "Spotify") {
+            icon = <SpotifyIcon width={22} height={22} color={Colors.white} />
+        } else if (item.name == "Amazon") {
+            icon = <AmazonIcon width={22} height={22} color={Colors.white} />
+        } else if (item.name == "Figma") {
+            icon = <FigmaIcon width={22} height={22} color={Colors.white} />
+        } else if (item.name == "Online Shopping") {
+            icon = <ShoppingCartIcon width={22} height={22} color={Colors.white} />
+        }
+        return(
+            <View style={styles.spendingWrapper} key={item.id}> 
+                <View style={styles.iconWrapper}>
+                    {icon}
+                </View>
+                <View 
+                style={styles.textWrapper}>
+                <View style={{gap:5}}>
+                <Text style={styles.itemName}>{item.name}</Text>
+                <Text style={{ color: Colors.white }}>{item.date}</Text>
+                </View>
+                <Text style={styles.itemName}>{item.amount}</Text>
+                </View>
+            </View>
+
+        );
+    })}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    spendingWrapper: {
+        flexDirection: 'row', 
+        alignItems:'center', 
+        marginVertical: 10
+    },
+    textWrapper: {
+        flex:1, 
+        flexDirection: 'row', 
+        justifyContent:'space-between', 
+        alignItems:'center'
+        },
+    iconWrapper: {
+        backgroundColor:Colors.grey, 
+        padding:15, 
+        borderRadius: 50, 
+        marginRight: 10
+    },
+    itemName: { 
+        color: Colors.white, 
+        fontSize: 16, 
+        fontWeight: '600' 
+    },
+    spendingSectionWrapper: {
+        marginVertical:20, 
+        alignItems:'flex-start'
+    },
+    sectionTitle: {
+        color: Colors.white, 
+        fontSize: 16, 
+        marginBottom: 15}
+})
