50f234bee0276b8075aad3459a6cee51d3624b2e
deedoth <ayoooladamilare@gmail.com>
2024-10-07 12:42:54 +0100

    added: types.ts

--- types.ts
+++ types.ts
@@ -5,3 +5,15 @@ export interface ExpenseType {
     percentage: string;
 }
 
+export interface IncomeType  {
+    id: string;
+    name: string;
+    amount: string;
+}
+
+export interface SpendingType  {
+    id: string;
+    name: string;
+    amount: string;
+    date: string;
+}
